bd988acb459175640db5f8a406bcfd8a382a506a
NeverDecaf <jtr9289@gmail.com>
2021-03-08 18:48:57 -0500

    fixes #3 by finishing changes started in #2.

--- update.py
+++ update.py
@@ -9,31 +9,31 @@ from pathlib import Path
 import subprocess
 import shutil
 from distutils.dir_util import copy_tree
+import psutil
 
 CHROMIUM_PATH = Path(os.getenv('PROGRAMDATA'),'Ungoogled Chromium')
 VERSION_FROM_TAG = re.compile('^v([\d\.]*)')
-import psutil
+
 class ChromiumUpdater:
     OWNER = 'macchrome'
     REPO = 'winchrome'
     def __init__(self):
         try:
-            CHROMIUM_PATH.mkdir()
+            CHROMIUM_PATH.mkdir() # can use exist_ok=True if pathlib > 3.5
         except FileExistsError:
             pass
-        sub_key_7zfm = 'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\App Paths\\7zFM.exe'
+        sub_key_7zfm = r'SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\7zFM.exe'
         try:
             with winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, sub_key_7zfm) as key_handle:
                 sevenzipfm_dir = winreg.QueryValueEx(key_handle, 'Path')[0]
         except OSError:
             raise Exception('Unable to locate 7-zip from the Windows Registry')
-        sevenzip_path = Path(sevenzipfm_dir, '7z.exe')
-        if not sevenzip_path.is_file():
-            raise Exception('7z.exe not found at path from registry: {}'.format(sevenzip_path))
-        self.SEVENZIP = sevenzip_path
+        self.SEVENZIP = Path(sevenzipfm_dir, '7z.exe')
+        if not self.SEVENZIP.is_file():
+            raise Exception(f'7z.exe not found at path from registry: {self.SEVENZIP}')
         
     def _get_latest_release(self):
-        r = requests.get('https://api.github.com/repos/{}/{}/releases'.format(self.OWNER, self.REPO))
+        r = requests.get(f'https://api.github.com/repos/{self.OWNER}/{self.REPO}/releases')
         r.raise_for_status()
         js = r.json()
         for release in sorted(js, key=lambda a:a['id'], reverse=True):
@@ -51,7 +51,8 @@ class ChromiumUpdater:
     def _check_running(self):
         for proc in psutil.process_iter():
             try:
-                if proc.name() == 'chrome.exe' and proc.exe() == str(Path(CHROMIUM_PATH,'chrome.exe')):
+                exepath = Path(CHROMIUM_PATH,'chrome.exe')
+                if proc.name() == exepath.name and proc.exe() == str(exepath):
                     raise Exception('Chromium is currently running.')
             except psutil.NoSuchProcess as err:
                 pass
@@ -62,24 +63,22 @@ class ChromiumUpdater:
         startup_key = r'Software\Microsoft\Windows\CurrentVersion\Run'
         winreg.CreateKeyEx(winreg.HKEY_CURRENT_USER, startup_key)
         key = winreg.OpenKeyEx(winreg.HKEY_CURRENT_USER, startup_key, access=winreg.KEY_WRITE)
-        winreg.SetValueEx(key, 'Ungoogled Chromium Updater', 0, winreg.REG_SZ, 'pyw "{}"'.format(path))
+        winreg.SetValueEx(key, 'Ungoogled Chromium Updater', 0, winreg.REG_SZ, f'pyw "{path}"')
         winreg.CloseKey(key)
 
     def update(self):
         self._check_running()
         new_version = self._get_latest_release()
-        version = [name for name in CHROMIUM_PATH.iterdir() if name.lower().endswith('manifest')]
-        if len(version):
-            version = os.path.splitext(version[0])[0]
-        else:
-            version = '0'
+        version = [path for path in CHROMIUM_PATH.iterdir() if path.suffix.lower() == '.manifest']
+        version = version[0].stem if version else '0'
         if version != new_version:
             print('New version found, updating...')
         else:
+            print('Ungoogled Chromium is up to date.')
             return
         tmpzip = Path(CHROMIUM_PATH,'zipped_tmp.7z')
         try:
-            tmpzip.unlink()
+            tmpzip.unlink() # can use missing_ok=True if pathlib > 3.8
         except FileNotFoundError:
             pass
         r = requests.get(self.DOWNLOAD_URL)
@@ -87,27 +86,29 @@ class ChromiumUpdater:
         tmpzip.write_bytes(r.content)
         si = subprocess.STARTUPINFO()
         si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+        dirs_before = set([f for f in CHROMIUM_PATH.iterdir() if f.is_dir()])
         try:
             output = subprocess.check_output([str(self.SEVENZIP), 'x', str(tmpzip), f'-o{CHROMIUM_PATH}','-y'], startupinfo=si)
         except subprocess.CalledProcessError:
             raise Exception('7zip extraction failed.')
-
+        dirs_after = set([f for f in CHROMIUM_PATH.iterdir() if f.is_dir()])
+        if len(dirs_after-dirs_before) != 1:
+            raise Exception('Unexpected contents of Chromium archive.')
+        googledir = (dirs_after-dirs_before).pop()
+        
         # delete all files in directory:
         for path in CHROMIUM_PATH.iterdir():
-            tmp_path = Path(CHROMIUM_PATH,path)
-            if tmp_path.is_dir():
-                if not path.startswith('ungoogled'):
-                    shutil.rmtree(tmp_path)
-                else:
-                    googledir = tmp_path
-            elif path not in (tmpzip.name,os.path.basename(__file__)):
-                tmp_path.unlink()
+            if path.is_dir():
+                if path != googledir:
+                    shutil.rmtree(path)
+            elif path.name not in (tmpzip.name,os.path.basename(__file__)):
+                path.unlink()
 
         # copy contents of folder.
         copy_tree(googledir, str(CHROMIUM_PATH))
         #cleanup
         try:
-            tmpzip.unlink()
+            tmpzip.unlink() # can use missing_ok=True if pathlib > 3.8
         except FileNotFoundError:
             pass
         shutil.rmtree(googledir)
@@ -124,3 +125,4 @@ if __name__ == '__main__':
     if args.install:
         shutil.copyfile(os.path.abspath(__file__),Path(CHROMIUM_PATH,os.path.basename(__file__)))
         c.run_on_windows_startup(path=Path(CHROMIUM_PATH,os.path.basename(__file__)))
+        print('Install successful, will run automatically on Windows startup.')
